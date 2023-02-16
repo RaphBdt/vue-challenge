38f2cd091970d6d3523d5df993b298d0567d5355
Raphaël Beaudet <raphaelbeaudet@icloud.com>
2023-02-16 11:36:53 +0100

    niveau 5

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,14 @@ export default {
   data() {
     return {
       firstname: "Jean",
-      age: 30
+      age: 30,
+      year: 2022,
     }
   },
   mounted() {
     setInterval(() => {
-      this.age++
+      this.age++;
+      this.year++;
     }, 3000);
   }
 }
@@ -20,7 +22,7 @@ export default {
       <p>Bonjour je m'appelle {{ firstname }}</p>
     </div>
     <div id="age">
-      <p>En 2022, {{ firstname }} aura {{ age }} ans</p>
+      <p>En {{ year }}, {{ firstname }} aura {{ age }} ans</p>
     </div>
   </div>
 </template>
